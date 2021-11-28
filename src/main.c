2304ade596858e0a07287db005bf1d22f9dc82a3
jnum <jnum@viessmann.com>
2021-11-28 18:24:49 +0100

    better moving average

--- src/main.c
+++ src/main.c
@@ -4,6 +4,13 @@
 #include "bsp/i2c_driver.h"
 #include "scd41/scd41.h"
 
+static int movingAvg(int *ptrArrNumbers, int *ptrSum, int pos, int len, int nextNum)
+{
+  *ptrSum = *ptrSum - ptrArrNumbers[pos] + nextNum;
+  ptrArrNumbers[pos] = nextNum;
+  return *ptrSum / len;
+}
+
 static volatile int32_t avgCo2 = 600; // initial value 
 void main(void)
 {
@@ -27,23 +34,36 @@ void main(void)
 	printk("s0: %d s1: %d s2: %d\n", s0, s1, s2);
 
 	scd41_start_periodic_measurement();
-	k_sleep(K_SECONDS(5U));
+
+	int arrNumbers[5] = {0};
+	int pos = 0;
+	int co2Avg = 0;
+	int sum = 0;
+	int len = sizeof(arrNumbers) / sizeof(int);
+	bool moving_averge_filled = false;
 
 	while(1)
 	{
 		uint16_t co2 = 0;
 		uint32_t temperature = 0;
 		uint16_t humidity = 0;
+		k_sleep(K_SECONDS(5U));
 		scd41_get_measures(&co2, &temperature, &humidity);
 		printk("co2 %d\n", co2);
 		printk("temperature %d\n",   temperature);
-		printk("humidity %d\n", humidity);
+		printk("humidity %d\n", humidity);		
+
 		if(co2 > 0)
 		{
-			avgCo2 = 0.9 * avgCo2 + 0.1 * co2;
+			co2Avg = movingAvg(arrNumbers, &sum, pos, len, co2);
 		}
-		printk("moving average co2 %d\n", avgCo2);
 
-		k_sleep(K_SECONDS(5U));
-	}
+		pos++;
+		if (pos >= len){
+			moving_averge_filled = true;
+			pos = 0;
+		}
+		if(moving_averge_filled)
+			printk("moving average co2 %d\n", co2Avg);
+		}
 }
